de5a3e2acbf58782fb33977387977e460fda805a
jennifer.G <jennifergunasekaran@gmail.com>
2026-06-22 19:06:33 +0530

    Update API URL

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,13 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+// export default API;
+
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+export default axios.create({
+  baseURL: "https://library-management-system-47by.onrender.com/api",
 });
-
-export default API;
